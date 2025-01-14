2fa430d90e72c1507257ec5461f6a1cb600183d3
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-14 13:50:05 +0530

    add api validations

--- server/controllers/DonorCircle.js
+++ server/controllers/DonorCircle.js
@@ -28,6 +28,20 @@ const postDonors =  async (req, res) => {
     try {
         const { name, mobile, address, bloodGroup } = req.body;
 
+        if (!name || !mobile || !address || !bloodGroup) {
+            const missingFields = [];
+            if (!name) missingFields.push("Name");
+            if (!mobile) missingFields.push("Mobile");
+            if (!address) missingFields.push("Address");
+            if (!bloodGroup) missingFields.push("Blood Group");
+        
+            return res.status(400).json({
+                success: false,
+                message: `Please enter the following fields: ${missingFields.join(", ")}`
+            });
+        }
+
+
         const newData = new Donor({
             name: name,
             mobile: mobile,
