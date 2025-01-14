df0523e39967f70b69ef65c521900df0351d235c
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-14 11:07:47 +0530

    Connect to mongoose Database

--- server/index.js
+++ server/index.js
@@ -2,30 +2,41 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 import { getHealth } from './controllers/Health.js';
 import { getInvaild } from './controllers/Invalid.js';
-import {getHome} from './controllers/DonorCircle.js';
+import { getHome } from './controllers/DonorCircle.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const ConnectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    if (conn) {
+        console.log('Connected to MongoDB')
+    }
+}
+
 
 //health API's
-app.get("/health",getHealth)
+app.get("/health", getHealth)
 
-//
-app.get("/",getHome)
+//App API's
+app.get("/", getHome)
+
 
 
 //Invalid API's
-app.get("*",getInvaild)
+app.get("*", getInvaild)
 
 
-const PORT = process.env.PORT || 5002 
+const PORT = process.env.PORT || 5002
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
+    ConnectDB();
 })
