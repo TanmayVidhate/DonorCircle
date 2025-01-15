6551dc296dc3084d5dceda29e8e298586b920a32
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-15 19:10:17 +0530

    add try and catch

--- client/src/views/Details.js
+++ client/src/views/Details.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 function Details() {
     const { userid } = useParams();
+
+    const [donorDetails, setDonorDetails] = useState({})
+    const LoadDetailOfDonors = async (userid) => {
+        try {
+            const response = await axios.get(`http://localhost:5002/Donors/${userid}`);
+            setDonorDetails(response?.data?.data);
+        }
+        catch (error) {
+            console.log(error?.response?.data?.message)
+        }
+    }
+
+    useEffect(() => {
+        LoadDetailOfDonors(userid);
+    }, [])
     return (
         <div>
             <p>Details Page</p>
-                {userid}
         </div>
     )
 }
