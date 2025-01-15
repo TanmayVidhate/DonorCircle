06dc68b21aa22d821c5f6da8e73afa8223c57fcd
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-15 13:31:36 +0530

    change path

--- client/src/views/Records.js
+++ client/src/views/Records.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
 
 function Records() {
+  const LoadData = async () => {
+    try {
+      const response = await axios.get("http://localhost:5002/Donors")
+      console.log(response?.data?.data)
+    }
+    catch (error) {
+      console.log(error?.message);
+    }
+  }
+
+  useEffect(() => {
+    LoadData();
+  }, [])
   return (
-    <div>Records</div>
+    <>
+      <p>Records</p>
+
+    </>
   )
 }
 
-export default Records
+export default Records;
