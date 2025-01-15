605134a7bbb2263e71af2519bcd6ebbee01151db
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-15 11:16:18 +0530

    add image

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import MainImag from '../assets/im'
 function Landing() {
     return (
         <>
-            <h2>Welcom to DonorCircle</h2>
+            <h2 className='text-center font-medium text-4xl m-2 text-primary'>DonorCircle</h2>
+
+            <h4 className='text-center text-2xl font-medium mt-1'>Save Lives,Donate Blood</h4>
+
+            <div className=''>
+                <img src={bloddImag} alt='image1'/>
+            </div>
         </>
     )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E63946', // Red for Action/Highlights
+        secondary: '#F8F9FA', // Light Gray for Background
+        accent: '#212529', // Dark Gray for Typography/Details
+        neutral: '#FFFFFF', // White for Borders/Background Elements
+      },
+    },
   },
   plugins: [],
 }
