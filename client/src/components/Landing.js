2117d37b08dbaa8d425589014615138ca7717eb4
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-15 10:09:28 +0530

    add routing in index.js

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <>
+            <h2>Welcom to DonorCircle</h2>
+        </>
+    )
+}
+
+export default Landing;
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import {createBrowserRouter,RouterProvider}  from 'react-router-dom';
+
+import Landing from './components/Landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router =createBrowserRouter([
+    {
+        path:"/",
+        element:<Landing/>
+    }
+])
 root.render(
-  
+    <RouterProvider router={router} />
 );
 
 
